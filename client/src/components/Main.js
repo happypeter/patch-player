7ea26e8e1f5bf8a2563f69571f3af223304853b3
Peter Wang <happypeter1983@gmail.com>
2018-01-24 09:17:00 +0800

    container/componnet #10

---
+++ client/src/components/Main.js
@@ -0,0 +1,80 @@
+import React, { Component } from 'react'
+import * as utils from '../utils/'
+
+class Main extends Component {
+  state = {
+    textLines: [
+      'the first line',
+      'the second line',
+      'the third line'
+    ]
+  }
+
+  typeCharacter = (character, index) => {
+    return new Promise((resolve) => {
+      let textLines = utils.insertCharacterAtIndex(this.state.textLines, character, index)
+      this.setState({ textLines }, () => {
+        const delay = 100
+        setTimeout(resolve, delay);
+      })
+    })
+  }
+
+  insertLine = (line, index) => {
+    const { textLines } = this.state
+    const newTextLines = utils.insertEmptyLineAtIndex(textLines, index)
+    this.setState({
+      textLines: newTextLines
+    })
+    // 先开辟出新行来，然后在下面的代码中在新的空白行中逐渐添加字符
+    utils.eachPromise(line, this.typeCharacter, index)
+  }
+
+  removeLine = lineIndex => {
+    const { textLines } = this.state
+    this.setState({
+      textLines: utils.addHintToDeletedLine(textLines, lineIndex)
+    })
+    setTimeout(
+      () =>
+        this.setState({
+          textLines: utils.removeElementAtIndex(textLines, lineIndex)
+        })
+      , 2000
+    )
+  }
+
+  handleInsert = () => {
+    const INSERT_LINE_INEXT = 1
+    const INSERT_LINE_TEXT = 'console.log(\'ss\')'
+    this.insertLine(INSERT_LINE_TEXT, INSERT_LINE_INEXT)
+  }
+
+  handleRemove = () => {
+    const REMOVED_LINE_INDEX = 0
+    this.removeLine(REMOVED_LINE_INDEX)
+  }
+
+  render() {
+    const { textLines } = this.state
+    const props = {
+      className: 'line'
+    }
+    const innerTree = textLines.map(
+      (t, i) => {
+        props.key = Math.random()
+        return React.createElement('div', props, t)
+      }
+    )
+    console.log('patch...', this.props.patch)
+    return (
+      <div>
+        {innerTree}
+        <button onClick={this.handleInsert}>Insert</button>
+        <button onClick={this.handleRemove}>Remove</button>
+      </div>
+    )
+  }
+}
+
+export default Main
