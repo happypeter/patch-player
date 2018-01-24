478f128991f78f52873bfc53df440edb17039ce8
Peter Wang <happypeter1983@gmail.com>
2018-01-24 14:44:11 +0800

    patch with two hunks #10

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,5 +1,6 @@
 const initialState = {
   patch: `@@ -2,7 +2,7 @@ import React, { Component } from 'react'
+ import styled from 'styled-components'
  import Typist from '../typist/Typist'
  import io from 'socket.io-client'
 -import Prism from 'prismjs'
@@ -7,6 +8,16 @@ const initialState = {
  import 'prismjs/components/prism-javascript'
  import 'prismjs/components/prism-markup'
  import 'prismjs/components/prism-jsx'
+@@ -22,12 +22,11 @@ class App extends Component {
+ 
+   componentDidMount = () => {
+     const socket = io(SERVER_URL);
+-    socket.on('file content and patch', data => {
+       const {files, commit} = this.state
++      hello peter
+       if (commit === data.commit) {
+         const exist = files.find(file => file.name === data.file.name)
+         if (!exist) {
   `
 }
 const rootReducer = (state = initialState, action) => {
