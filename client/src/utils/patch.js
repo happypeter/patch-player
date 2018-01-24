66b8f77ccbd92cab397020e8bd888f4372b4fb81
Peter Wang <happypeter1983@gmail.com>
2018-01-24 14:52:15 +0800

    parse Hunk

--- client/src/utils/patch.js
+++ client/src/utils/patch.js
@@ -6,17 +6,20 @@ import {
 import * as mutationTypes from '../constants/MutationTypes'
 
 
-
 export const parse = patch => {
+  return parseHunk(patch)
+}
+
+export const parseHunk = hunk => {
   let mutations = []
-  let hunkLines = patch.split('\n')
+  let hunkLines = hunk.split('\n')
   let metaMatches = hunkLines[0].match(HUNK_META)
   let startLineNum = Number(metaMatches[1])
 
-  const patchLines = hunkLines.slice(1)
+  const mainLines = hunkLines.slice(1)
   let offSet = 0
-  for (let i = 0; i < patchLines.length; i++) {
-    let line = patchLines[i]
+  for (let i = 0; i < hunkLines.length; i++) {
+    let line = mainLines[i]
     if (DELETED_LINE.test(line)) {
       mutations.push({
         type: mutationTypes.DELETE,
