e9ea9beec308746b400c4007329dfb5171bdc58e
Peter Wang <happypeter1983@gmail.com>
2018-01-24 22:48:36 +0800

    combineReducers #12

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,26 +1,6 @@
-const initialState = {
-  patch: `@@ -2,7 +2,7 @@ import React, { Component } from 'react'
- import styled from 'styled-components'
--import Typist from '../typist/Typist'
--import io from 'socket.io-client'
--import Prism from 'prismjs'
-+import Prsm from 'prismjs'
- import 'prismjs/components/prism-javascript'
- import 'prismjs/components/prism-markup'
- import 'prismjs/components/prism-jsx'
-@@ -22,12 +22,11 @@ class App extends Component {
-   componentDidMount = () => {
-     const socket = io(SERVER_URL);
--    socket.on('file content and patch', data => {
--      const {files, commit} = this.state
-+      hello peter
-       if (commit === data.commit) {
-         const exist = files.find(file => file.name === data.file.name)
-         if (!exist) {
-  `
-}
-const rootReducer = (state = initialState, action) => {
-  return state
-}
+import { combineReducers } from 'redux'
+import patch from './patch'
 
-export default rootReducer
+export default combineReducers({
+  patch
+})
