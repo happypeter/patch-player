d985097a145899055349ef73d75bfd63f0df5a87
Peter Wang <happypeter1983@gmail.com>
2018-01-24 09:34:28 +0800

    patch in store #10

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,5 +1,15 @@
 const initialState = {
-  patch: 'hhh'
+  patch: `
+@@ -2,7 +2,7 @@ import React, { Component } from 'react'
+ import styled from 'styled-components'
+ import Typist from '../typist/Typist'
+ import io from 'socket.io-client'
+-import Prism from 'prismjs'
++import Prsm from 'prismjs'
+ import 'prismjs/components/prism-javascript'
+ import 'prismjs/components/prism-markup'
+ import 'prismjs/components/prism-jsx'
+  `
 }
 
 const rootReducer = (state = initialState, action) => {
