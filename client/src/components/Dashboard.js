471683ac36e4fec81b9e810491ef6e4022617ea3
Peter Wang <happypeter1983@gmail.com>
2018-01-25 20:24:15 +0800

    fix #15

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -2,11 +2,12 @@ import React, { Component } from 'react'
 import styled from 'styled-components'
 
 class Dashboard extends Component {
+  // TODO: 选择项目，项目文件夹传递给后端 express 代码
+  // TODO: 启动/暂停代码播放
   render () {
     return (
       <Wrap>
-        <button onClick={this.props.setProjectPath}>
-          选择项目
+        <button onClick={this.props.setProjectPath}> 选择项目
         </button>
       </Wrap>
     )
