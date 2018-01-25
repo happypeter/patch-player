0efe20cc46dfd7e6ccc821404f278a4a24b6e418
Peter Wang <happypeter1983@gmail.com>
2018-01-25 16:37:47 +0800

    prismjs #13

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -1,14 +1,21 @@
 import React, { Component } from 'react'
+import Prism from 'prismjs'
+import 'prismjs/components/prism-jsx'
+import '../assets/global.css'
 
 class Main extends Component {
+  componentDidUpdate() {
+    Prism.highlightAll()
+  }
+
   render() {
     const { textLines } = this.props
     const props = {
-      className: 'line'
+      className: 'language-jsx'
     }
     const innerTree = textLines.map((t, i) => {
       props.key = Math.random()
-      return React.createElement('div', props, t)
+      return React.createElement('code', props, t)
     })
     return (
       <div>
