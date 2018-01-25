5d7c61ade46ebe1cb37d0dcfc62cdb791fc2b9c6
Peter Wang <happypeter1983@gmail.com>
2018-01-25 21:16:24 +0800

    socket.io

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react'
 import styled from 'styled-components'
+import io from 'socket.io-client'
+const socket = io('http://localhost:3002')
 
 class Dashboard extends Component {
   // TODO: 选择项目，项目文件夹传递给后端 express 代码
   // TODO: 启动/暂停代码播放
+  componentDidMount() {
+    socket.on('git commits', data => {
+      console.log('git commits', data)
+    })
+  }
+  handleClick = (e) => {
+    socket.emit('repo', {repo: 'patch-typist'})
+  }
   render () {
     return (
       <Wrap>
+        <button onClick={this.handleClick}>socket</button>
         <button onClick={this.props.setProjectPath}> 选择项目
         </button>
       </Wrap>
---
+++ server/index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const http = require('http')
+const socketIo = require('socket.io')
+const app = express()
+const server = http.createServer(app)
+const io = socketIo(server)
+
+io.on('connection', socket => {
+  socket.on('repo', source => {
+    console.log(source)
+    socket.broadcast.emit('git commits', {commits: 'commits'})
+  })
+})
+
+server.listen(3002, () => {
+  console.log('running on port 3002...')
+})
