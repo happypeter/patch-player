b741a059027f90f98de4899d7e2214310ed090b3
Peter Wang <happypeter1983@gmail.com>
2018-01-25 14:53:33 +0800

    refactor #12

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -3,48 +3,6 @@ import * as utils from '../utils/'
 import * as patch from '../utils/patch'
 
 class Main extends Component {
-  state = {
-    textLines: []
-  }
-
-  componentWillMount() {
-    const file = `import styled from 'styled-components'
- import Typist from '../typist/Typist'
- import io from 'socket.io-client'
- import Prism from 'prismjs'
- import 'prismjs/components/prism-javascript'
- import 'prismjs/components/prism-markup'
- import 'prismjs/components/prism-jsx'
- dsds
- sdsd
- sd
- sd
- sd
- sds
- ds
- d
- d
- d
- d
- d
- d
-   componentDidMount = () => {
-     const socket = io(SERVER_URL);
-    socket.on('file content and patch', data => {
-      const {files, commit} = this.state
-       if (commit === data.commit) {
-         const exist = files.find(file => file.name === data.file.name)
-         if (!exist) {
-    
-    `
-
-    const textLines = file.split('\n')
-    this.setState({
-      textLines
-    })
-  }
-
-
   applyMutation = mutation => {
     if(mutation.type === 'DELETE') return this.props.removeLine(mutation)
     return this.props.insertLine(mutation)
@@ -55,7 +13,6 @@ class Main extends Component {
   }
 
   render() {
-    console.log('this.props.textLines..', this.props.textLines)
     const { textLines } = this.props
     const props = {
       className: 'line'
