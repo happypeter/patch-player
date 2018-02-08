c253d14ed904794d8216cc0bbadd84c5d5a34430
Peter Wang <happypeter@users.noreply.github.com>
2018-02-08 09:26:25 +0800

    Update sync-middleware.js

--- client/src/utils/sync-middleware.js
+++ client/src/utils/sync-middleware.js
@@ -1,7 +1,6 @@
 export const createSyncMiddleware = socket => {
   return () => next => action => {
     if (typeof action === 'object') {
-      // FIXME: 这里的 action 首次收到的是一个 function ，大概是 thunk 造成的
       socket.emit('action', action)
     }
     next(action)
